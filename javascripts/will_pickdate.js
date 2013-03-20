cfa0ecc8426d7dae9673c8455f10b98b0894d428
Fábio <fbatista@webreakstuff.com>
2013-03-20 22:40:54 +0700

    ..

--- javascripts/will_pickdate.js
+++ javascripts/will_pickdate.js
@@ -744,6 +744,7 @@
           case 'i': f += this.leadZero(t.getMinutes()); break;
           case 's': f += this.leadZero(t.getSeconds()); break;
           case 'U': f += Math.floor(t.valueOf() / 1000); break;
+          case 'S': f += t.toISOString(); break;
           default:  f += format.charAt(i);
         }
       }
